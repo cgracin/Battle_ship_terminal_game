d690c91be6b541ed3ae0c3663e56fa486b41990c
Carter Gracin <carter.rgracin@gmail.com>
2021-12-24 20:36:59 -0600

    Functionality to create a board size

--- BattleShip.py
+++ BattleShip.py
@@ -1,4 +1,16 @@
 board = []
 
-size = input("Enter board size")
-print(size)
+size = input("Enter board size: ")
+
+size = int(size)
+
+for i in range(0, size):
+    board.append(["O"] * size)
+
+
+def print_board(board):
+    for i in board:
+        print(" ".join(i))
+
+
+print_board(board)
