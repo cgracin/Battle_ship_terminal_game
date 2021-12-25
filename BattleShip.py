0268692543f584a2b25b9aeeb98f17cae920bd14
Carter Gracin <carter.rgracin@gmail.com>
2021-12-24 22:43:58 -0600

    Added function to create boards

--- BattleShip.py
+++ BattleShip.py
@@ -1,33 +1,22 @@
-
-from random import randint
-
-board = []
-
-# print("Hello!!")
-# print("Welcome to BattleShip")
-
-size = input("Enter board size: ")
-
-size = int(size)
-
-for i in range(0, size):
-    board.append(["O"] * size)
+import random
 
 
-def print_board(board):
-    for i in board:
-        print(" ".join(i))
+class GameBoard:
+    def __init__(self, board):
+        self.board = board
+
+    def get_letters_to_numbers():
+        letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+        return letters_to_numbers
+
+    def print_board(self):
+        print("  A B C D E F G H")
+        print("  +-+-+-+-+-+-+-+")
+        row_number = 1
+        for row in self.board:
+            print("%d|%s|" % (row_number, "|".join(row)))
+            row_number += 1
 
 
-# print_board(board)
-
-
-def place_ship(board):
-    x_var = randint(0, size - 1)
-    y_var = randint(0, size - 1)
-    print(x_var, y_var)
-    board[x_var][y_var] = "X"
-    # print_board(board)
-
-
-place_ship(board)
+user_board = GameBoard([[" "] * 8 for i in range(8)])
+GameBoard.print_board(user_board)
