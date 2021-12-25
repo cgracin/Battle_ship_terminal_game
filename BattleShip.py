9736d0d702b7aa8f5a3b6331dab778c09551972a
Carter Gracin <carter.rgracin@gmail.com>
2021-12-24 22:59:02 -0600

    Created battleship class to allow creation of gameboard, user input, and hit counter

--- BattleShip.py
+++ BattleShip.py
@@ -18,5 +18,43 @@ class GameBoard:
             row_number += 1
 
 
-user_board = GameBoard([[" "] * 8 for i in range(8)])
-GameBoard.print_board(user_board)
+class Battleship:
+    def __init__(self, board):
+        self.board = board
+
+    def create_ships(self):
+        for i in range(5):
+            self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            while self.board[self.x_row][self.y_column] == "X":
+                self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            self.board[self.x_row][self.y_column] = "X"
+        return self.board
+
+    def get_user_input(self):
+        try:
+            x_row = input("Enter the row of the ship: ")
+            while x_row not in '12345678':
+                print("Not an appropriate choice, please select a valid row")
+                x_row = input("Enter the row of the ship: ")
+
+            y_column = input("Enter the column letter of the ship: ").upper
+            while y_column not in "ABCDEFGH":
+                print("Not an appropriate choice, please select a valid column")
+                y_column = input("Enter the column letter of the ship: ").upper
+
+        return int(x_row) - 1, GameBoard.get_letters_to_numbers()[y_column]
+
+        except ValueError and KeyError:
+            print("Not a valid input")
+            return self.get_user_input()
+
+    def count_hit_ships(self):
+        hit_ships = 0
+        for row in self.board:
+            for column in row:
+                if column == "X":
+                    hit_ships += 1
+        return hit_ships
+
+# user_board = GameBoard([[" "] * 8 for i in range(8)])
+# GameBoard.print_board(user_board)
